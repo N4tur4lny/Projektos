f381f575d070176cb9388594e50dac00a8beedbe
Unknown <sneaky517@gmail.com>
2018-04-15 17:52:40 +0200

    Create window

--- Projekt_angielski/Projekt_angielski/Program.cs
+++ Projekt_angielski/Projekt_angielski/Program.cs
@@ -15,8 +15,7 @@ namespace Projekt_angielski
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Window());
         }
     }
 }
---
+++ Projekt_angielski/Projekt_angielski/Window.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace Projekt_angielski
+{
+    class Window : Form
+    {
+        public Window()
+        {
+            Height = 600;
+            Width = 800;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Text = "Projektos";
+        }
+
+
+    }
+}
