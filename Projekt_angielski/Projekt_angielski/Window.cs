32d336e6e9cab0b9c254b5a0bca875f05bc0850b
Unknown <sneaky517@gmail.com>
2018-05-15 20:55:09 +0200

    v0.1

--- Projekt_angielski/Projekt_angielski/Window.cs
+++ Projekt_angielski/Projekt_angielski/Window.cs
@@ -12,16 +12,60 @@ namespace Projekt_angielski
 {
     class Window : Form
     {
+        Dictionary<string, string> words = new Dictionary<string, string>()
+        {
+            {"Word", "słowo" },
+            {"Car", "auto" }
+        };
+
         public Window()
         {
+            Random random = new Random();
+            int randomNumber = random.Next(0, words.Count);
             Height = 600;
             Width = 800;
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
             MinimizeBox = false;
             Text = "Projektos";
+
+            Button submitButton = new Button();
+            submitButton.Text = "Submit";
+            submitButton.Location = new Point(350, 320);
+            this.Controls.Add(submitButton);
+
+            TextBox outText = new TextBox();
+            outText.Width = 200;
+            outText.Height = 50;
+            outText.Location = new Point(300, 200);
+            this.Controls.Add(outText);
+
+            TextBox inText = new TextBox();
+            inText.Width = 200;
+            inText.Height = 50;
+            inText.Location = new Point(300, 300);
+            this.Controls.Add(inText);
+
+            outText.Text = words.Keys.ElementAt(randomNumber);
+            string value = outText.Text;
+            string corrAnswer = words[value];
+
+            submitButton.Click += (o, i) =>
+            {
+                string answer = inText.Text;
+                if (answer != null)
+                {
+                    if (answer == corrAnswer)
+                    {
+                        outText.Text = "Correct";
+                    }
+
+                    else if (answer != corrAnswer)
+                    {
+                        outText.Text = "Incorrect";
+                    }
+                }
+            };
         }
-
-
     }
 }
